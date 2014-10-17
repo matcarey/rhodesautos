ff94f4abec462ae52f46c4a96da19bfa56b9a294
Mat Carey <mat@matcarey.co.uk>
2014-10-17 22:08:01 +0100

    initial version.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/**
+ * Gruntfile.js
+ */
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-composer');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    php: {
+      dist: {
+        options: {
+          port: 8000,
+          base: 'web',
+          open: true
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'web/src/style.css' : 'style/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'style/style.scss',
+        tasks: ['sass']
+      }
+    },
+    composer : {
+      options : {
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-php');
+  grunt.loadNpmTasks('grunt-phplint');
+  grunt.loadNpmTasks('grunt-phpunit');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('dev', ['sass', 'php', 'watch']);
+  grunt.registerTask('build', ['sass', 'composer:install']);
+};
